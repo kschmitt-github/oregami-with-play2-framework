bafadb3ff5eac6e2f3fc1f14019728f71a72f62c
gene-oregami <sebastian@kultpower.de>
2013-02-28 13:44:08 +0100

    email validation

--- play2-app/app/org/oregami/util/validation/UserValidator.java
+++ play2-app/app/org/oregami/util/validation/UserValidator.java
@@ -3,6 +3,9 @@ package org.oregami.util.validation;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
 import org.apache.commons.lang3.StringUtils;
 import org.oregami.data.UserDao;
 import org.oregami.entities.user.User;
@@ -34,7 +37,11 @@ public class UserValidator {
 
         if (StringUtils.isEmpty(userData.getUsername())) {
             errorMessages.add(new ServiceError(new ServiceErrorContext("username"), "org.oregami.user.username.isEmpty"));
-        }
+        } else {
+	        if (userData.getUsername().length()<3) {
+	        	errorMessages.add(new ServiceError(new ServiceErrorContext("username"), "org.oregami.user.username.minimumLengthViolation"));
+	        }
+    	}
 
         if (StringUtils.isEmpty(userData.getEmail())) {
         	errorMessages.add(new ServiceError(new ServiceErrorContext("email"), "org.oregami.user.email.isEmpty"));
@@ -42,8 +49,23 @@ public class UserValidator {
 
         if (StringUtils.isEmpty(userData.getPassword())) {
         	errorMessages.add(new ServiceError(new ServiceErrorContext("password"), "org.oregami.user.password.isEmpty"));        	
+        } else {
+        	if (!isValidEmailAddress(userData.getEmail())) {
+            	errorMessages.add(new ServiceError(new ServiceErrorContext("email"), "org.oregami.user.email.notValid"));        	
+            }
         }
 
         return errorMessages;
     }
+    
+	private static boolean isValidEmailAddress(String email) {
+		boolean result = true;
+		try {
+			InternetAddress emailAddr = new InternetAddress(email);
+			emailAddr.validate();
+		} catch (AddressException ex) {
+			result = false;
+		}
+		return result;
+	}  
 }
