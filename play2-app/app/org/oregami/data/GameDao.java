3d94ff235d4746f0406a82568aff78ef8923ed9a
gene-oregami <sebastian@kultpower.de>
2013-03-07 00:38:27 +0100

    lowercase findByName

--- play2-app/app/org/oregami/data/GameDao.java
+++ play2-app/app/org/oregami/data/GameDao.java
@@ -19,7 +19,7 @@ public class GameDao extends GenericDAOImpl<Game, Long>{
     @SuppressWarnings("unchecked")
 	public List<Game> findByName(String name) {
         List<Game> games = (List<Game>)getEntityManager()
-        		.createNativeQuery("SELECT * FROM Game g, GameTitle t where g.id=t.GameId and t.title like '%" + name + "%'", Game.class).getResultList(); 
+        		.createNativeQuery("SELECT * FROM Game g, GameTitle t where g.id=t.GameId and lower(t.title) like '%" + name.toLowerCase() + "%'", Game.class).getResultList(); 
         return games;
     }
 
